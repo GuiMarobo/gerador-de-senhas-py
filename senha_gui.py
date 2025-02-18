087cd7692edb2801dfd467de556fb020bb8eea94
Guilherme Marobo <guimarobo@gmail.com>
2025-02-18 03:09:11 -0300

    subir arquivo gui

---
+++ senha_gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+janela = tk.Tk()
+janela.title('Gerador de Senhas - by: @GuiMarobo')
+
+titulo = tk.Label(janela, text='Gerador de Senhas', font=('Arial', 16, "bold"))
+titulo.pack(pady=10)
+
+janela.mainloop()
